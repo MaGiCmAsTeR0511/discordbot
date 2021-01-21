2fef07f43b49c3cdf0f7b0eadbf8667f0ae14685
MaGiCmAsTeR0511 <kurt@hohenauers.eu>
2021-01-21 19:21:39 +0100

    added some functions made a new function for all inco0mming mesages

--- bot.js
+++ bot.js
@@ -3,12 +3,40 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const utf8 = require('utf8');
 const stdin = process.stdin;
+const cf = require('./config.json');
+const prefix = cf.prefix;
 
 client.on('ready', () => {
 console.log(`${client.user.tag} is ready!`);
 });
 
 client.login(auth.token);
+
+
+client.on("message", function (message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
+    else if (command === "sum") {
+        const numArgs = args.map(x => parseFloat(x));
+        const sum = numArgs.reduce((counter, x) => counter += x);
+        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+    }
+    else if (command === "intro") {
+        message.reply('Hello');
+    }
+    else if (command === "codcw") {
+        message.reply('Hier ist mein battleTag für COD-COLDWAR:');
+    }
+});
 
 // Willkommen nachricht wenn neue Mitglieder dazu kommen
 client.on('guildMemberAdd', member => {
@@ -25,51 +53,11 @@ client.on('guildMemberRemove', member => {
     channel.send(`Der Drache wurde entfernt aus der Hoehle, ${member}`);
 });
 
-client.on('message', msg => {
-if (msg.content === '!intro') { 
-msg.reply('Hallo!');
-}
-});
-
-client.on('message', message => {
-    // If the message is "how to embed"
-    if (message.content === 'how to embed') {
-        // We can create embeds using the MessageEmbed constructor
-        // Read more about all that you can do with the constructor
-        // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-        const embed = new Discord.MessageEmbed()
-            // Set the title of the field
-            .setTitle('A slick little embed')
-            // Set the color of the embed
-            .setColor(0xff0000)
-            // Set the main content of the embed
-            .setDescription('Hello, this is a slick embed!');
-        // Send the embed to the same channel as the message
-        message.channel.send(embed);
-    }
-});
-
-client.on('message', message => {
-    // If the message is "what is my avatar"
-    if (message.content === 'what is my avatar') {
-        // Send the user's avatar URL
-        message.reply(message.author.displayAvatarURL());
-    }
-});
-
-client.on('message', msg => {
-  if (msg.content
-  === '!Wie ist das wetter?')
-  {
-      msg.reply(utf8.encode('In der Drachenhöhle ist es schön!'));
-  }
-});
-
 stdin.on('keypress', function (letter, key) {
   if (key
   && key.ctrl
   && key.name
-  == 'c')
+  === 'c')
   {
     process.exit();
   }

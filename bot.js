5752c68065db8c8112190ec1b6b422dc0ee67a2b
MaGiCmAsTeR0511 <kurt@hohenauers.eu>
2021-01-26 20:23:21 +0100

    added mongodb zugriff in bot js and testing emojis

--- bot.js
+++ bot.js
@@ -1,10 +1,12 @@
-const Discord = require('discord.js');
+﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./auth.json');
 const utf8 = require('utf8');
 const stdin = process.stdin;
 const cf = require('./config.json');
 const prefix = cf.prefix;
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 client.on('ready', () => {
 console.log(`${client.user.tag} is ready!`);
@@ -34,7 +36,25 @@ client.on("message", function (message) {
         message.reply('Hello');
     }
     else if (command === "codcw") {
-        message.reply('Hier ist mein battleTag f�r COD-COLDWAR:');
+        message.reply('Hier ist mein battleTag für COD-COLDWAR:');
+    }
+    else if (command === "anmeldung") {
+        if (args !== '') {
+            var user = message.member;
+            MongoClient.connect(url, function (err, db) {
+                if (err) throw err;
+                var dbo = db.db("discord");
+                var myobj = { _user:user.toString() , datum: args[0]};
+                dbo.collection("discord").insertOne(myobj, function (err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                    db.close();
+                });
+            });
+            message.reply(`Deine Anmeldung wurde bestätigt 🤩`);
+        } else {
+            message.reply('Du hast kein Datum für die ANmeldung eingegeben');
+        }
     }
 });
 
